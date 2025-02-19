074fcc491d0cb36fcd85134b3a266fd37ff59580
aryh.mothieu <aryh.mothieu@gmail.com>
2025-02-19 23:18:49 +0100

    Correcting UserDto authorities type

--- src/main/java/fr/perpetualmotion/airbnbback/user/application/dto/ReadUserDTO.java
+++ src/main/java/fr/perpetualmotion/airbnbback/user/application/dto/ReadUserDTO.java
@@ -5,6 +5,6 @@ import fr.perpetualmotion.airbnbback.user.domain.Authority;
 import java.util.Set;
 import java.util.UUID;
 
-public record ReadUserDTO(UUID publicId, String firstName, String lastName, String email, String imageUrl, Set<Authority> authorities) {
+public record ReadUserDTO(UUID publicId, String firstName, String lastName, String email, String imageUrl, Set<String> authorities) {
 
 }
